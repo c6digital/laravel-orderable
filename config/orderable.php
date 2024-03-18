e814136024aaa86739bfaeb25c315ee5832d98a6
Ryan Chandler <ryangjchandler@gmail.com>
2024-03-18 15:48:34 +0000

    wip

--- config/orderable.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// config for C6Digital/Orderable
-return [
-
-];
--- src/OrderableServiceProvider.php
+++ src/OrderableServiceProvider.php
@@ -10,16 +10,6 @@ class OrderableServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('laravel-orderable')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel-orderable_table')
-            ->hasCommand(OrderableCommand::class);
+        $package->name('laravel-orderable');
     }
 }
